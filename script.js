074285d350e5ed78e0af91e7cdee94735a73609c
Toye Ogundepo <ogundepo@gmail.com>
2020-10-03 00:03:05 -0500

    added inital shopping logic. Selecting a size and adding functionality to 'add to cart' button. Also added all product names. Need to verify womens product 7 and 8 (images).

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let sizeBox = document.querySelectorAll(".size-box");
 let addToCartButton = document.querySelector(".add-to-cart");
+let shoeSizeWarning = document.querySelector(".select-shoe-size");
+let nameOfShoe = document.querySelector(".name-of-shoe");
 
 let cart = [];
 for (let i = 0; i < sizeBox.length; i++) {
@@ -8,10 +10,16 @@ for (let i = 0; i < sizeBox.length; i++) {
         console.log(sizeBox[i].textContent);
         cart.push(sizeBox[i].textContent);
     })
+    
 }
 
 addToCartButton.addEventListener("click", function() {
-    console.log("hi");
+    if (cart.length < 1) {
+        shoeSizeWarning.textContent = "Please select a size.";
+    } else {
+        shoeSizeWarning.style.display = "none";
+        console.log(`${nameOfShoe.textContent}, size ${cart[cart.length - 1]} added to cart!`)
+    }
 })
 
 

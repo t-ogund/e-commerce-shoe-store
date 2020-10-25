5493c26425abf0a072acbf52b08c26b733ab0eca
Toye Ogundepo <ogundepo@gmail.com>
2020-10-24 19:46:34 -0500

    cart is now working properly.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ let cartEmpty = document.querySelector(".cart-empty");
 let cartEmptyV2 = document.querySelector(".the-cart-is-now-empty");
 let orderSummaryBreakdown = document.querySelector(".order-summary-breakdown");
 let total = document.querySelector(".total");
+let uniqueID = document.querySelector(".unique-id");
 
 let cart = [];
 for (let i = 0; i < sizeBox.length; i++) {
@@ -25,12 +26,15 @@ for (let i = 0; i < sizeBox.length; i++) {
   });
 }
 
+let productHolder = [];
+
 for (let i = 0; i < addToCartButton.length; i++) {
-  addToCartButton[i].addEventListener("click", function () {
+  addToCartButton[i].addEventListener("click", function (e) {
     if (cart.length < 1) {
       shoeSizeWarning.textContent = "Please select a size.";
       addToCartButton[i].removeAttribute("data-toggle");
     } else {
+      console.log(e)
       shoeSizeWarning.style.display = "none";
       addToCartButton[i].setAttribute("data-toggle", "modal");
       modalProductName.textContent = nameOfShoe.textContent;
@@ -38,28 +42,35 @@ for (let i = 0; i < addToCartButton.length; i++) {
       modalPrice.textContent = actualProductPrice.textContent;
       modalSize.textContent = `Size: ${cart[cart.length - 1]}`;
 
+      productHolder.push(itemID)
+      console.log(productHolder.length);
+
+      let uniqueIdValue = 0;
       let shoeObj = {
         productName: `${modalProductName.textContent}`,
         productSize: `${modalSize.textContent}`,
         productSex: `${modalSex.textContent}`,
         productPrice: `${modalPrice.textContent}`,
         productImage: gender.textContent === "Men's" ? `img/men-${itemID.textContent}.jpg` : `img/women-${itemID.textContent}.jpg`,
-        productID: `${itemID.textContent}` 
+        productID: `${itemID.textContent}`,
+        uniqueID: `${e.timeStamp}`
       };
+
       cartArray = JSON.parse(localStorage.getItem("shoe")) || [];
       cartArray.push(shoeObj);
       localStorage.setItem("shoe", JSON.stringify(cartArray));
-      // console.log("Cart Array: ", cartArray[0].productPrice);
-      console.log(shoeObj);
+
+      productHolderArray = JSON.parse(localStorage.getItem("productHolder")) || [];
+      productHolderArray.push(i);
+      localStorage.setItem("productHolder", JSON.stringify(productHolderArray));
     }
   });
+  
 }
 
 let summaryListItems = document.querySelector(".summary-list-items");
 
 let myCart = JSON.parse(localStorage.getItem("shoe"));
-// console.log("My Cart: ", myCart);
-
 
 for (let i = 0; i < myCart.length; i++) {
   let cartRowItem = document.createElement("div");
@@ -78,9 +89,7 @@ for (let i = 0; i < myCart.length; i++) {
   cartRowItem.appendChild(newCartRowColOne);
   cartRowItem.appendChild(newCartRowColTwo);
   }
-  // console.log("My Cart: ", myCart, myCart.length);
-
-
+ 
 let cartRightCol = document.querySelectorAll(".cart-right-column");
 for (let i = 0; i < cartRightCol.length; i++) {
   // product name
@@ -103,8 +112,8 @@ for (let i = 0; i < cartRightCol.length; i++) {
   deleteButtonSection.classList.add("delete-button-section");
   let deleteButton = document.createElement("button");
   deleteButton.textContent = "Remove Item";
-  deleteButton.classList.add("delete-button", "btn", "btn-danger");
-  deleteButton.setAttribute("data-id", myCart[i].productID);
+  deleteButton.classList.add("delete-button", "btn", "btn-danger", `unique-${i}`);
+  deleteButton.setAttribute("data-id", myCart[i].uniqueID);
   deleteButton.addEventListener("click", remove);
   deleteButtonSection.appendChild(deleteButton);
   cartRightCol[i].appendChild(deleteButton);
@@ -114,6 +123,7 @@ for (let i = 0; i < cartLeftCol.length; i++) {
     let cartLeftColImg = document.createElement("img");
     cartLeftColImg.classList.add("cart-left-col-img");
     cartLeftCol[i].appendChild(cartLeftColImg);
+    cartLeftCol[i].classList.add(`unique-${i}`);
 }
 
 let cartLeftColNodeList = document.querySelectorAll(".cart-left-col-img");
@@ -124,7 +134,6 @@ let cartLeftColNodeList = document.querySelectorAll(".cart-left-col-img");
 let cartDeleteButton = document.querySelectorAll(".delete-button");
 let cartRowItems = document.querySelectorAll(".cart-row-item");
 
-
 console.log("Cart Row Items: ", cartRowItems, cartRowItems.length);
 if (cartRowItems.length > 0) {
   cartEmpty.style.display = "none";
@@ -134,116 +143,54 @@ if (cartRowItems.length > 0) {
   cartEmpty.style.display = "block";
   orderSummaryBreakdown.classList.add("hide");
 }
-// if (cartEmpty.style.display === "block") {
-//   orderSummaryBreakdown.style.display = "none";
-// }
 
-
-// function retrieveItems() {
-  // let toArrayOfObjects = JSON.parse(retrievedMyCartItems);
-// }
-
-
-// let newToArrayOfObjects;
-// function deleteItem() {
-//   for (let q = 0; q < toArrayOfObjects.length; q++) {
-    
-//   }
-// }
 function remove(e) {
   let retrievedMyCartItems = localStorage.getItem("shoe");
   let toArrayOfObjects = JSON.parse(retrievedMyCartItems);
     console.log(e)
-    let newToArrayOfObjects = toArrayOfObjects.filter(item => e.target.parentNode.childNodes[0].textContent !== item.productName);
-    console.log("IT'S A MATCH!");
-    console.log(e.target.parentNode.childNodes);
-    console.log(toArrayOfObjects[0].productName);
+
+    //productHolderArray
+    let retrievedProductHolderItems = localStorage.getItem("productHolder");
+    let toArrayOfIndexes = JSON.parse(retrievedProductHolderItems);
+    console.log("Array of Indexes: ", toArrayOfIndexes);
+
+    // filter method (works: line 2)
+    // let newToArrayOfObjects = toArrayOfObjects.filter(item => e.target.parentNode.childNodes[0].textContent !== item.productName);
+
+    //This should be it!
+    let newToArrayOfObjects = toArrayOfObjects.filter(item => e.target.dataset.id !== item.uniqueID);
+  
+    for (let w = 0; w < cartDeleteButton.length; w++) {
+      let buttonUnique = e.target.classList[3];
+      let imageUnique = e.target.parentNode.parentNode.childNodes[0].classList[2];
+
+      if (buttonUnique === imageUnique) {
+        let parentToDelete = e.target.parentNode.parentNode;
+        parentToDelete.remove();
+      }
+    }
+
     localStorage.setItem("shoe", JSON.stringify(newToArrayOfObjects));
-    console.log(toArrayOfObjects);
     return toArrayOfObjects
 }
-// retrieveItems();
 
-// for (let d = 0; d < cartDeleteButton.length; d++) {
-//   cartDeleteButton[d].addEventListener("click", remove)
-  // console.log("hi")
-    // console.log(toArrayOfObjects);
-    // console.log(cartDeleteButton);
-    // remove();
-    // console.log(e);
-    // console.log(e.target.parentNode.childNodes[0].textContent)
-    // console.log(e.target.parentNode.parentNode);
-    // let valueToRemove = cartDeleteButton[d];
-    // console.log("Value to remove: ", d);
-  // newToArrayOfObjects = toArrayOfObjects.filter(item => e.target.parentNode.childNodes[0].textContent !== item.productName);
-  // console.log("New To Array Of Objects: ", newToArrayOfObjects)
-  
-    // console.log("Hi, from: ", d);
-    // console.log(toArrayOfObjects[d]);
-    // toArrayOfObjects.splice(d, 1);
-    // console.log(toArrayOfObjects);
-   
-    
-    // console.log("What was Spliced: ", whatWasSpliced, d);
-    // console.log("Updated Array: ", toArrayOfObjects);
-    // cartRowItems[d].style.display = "none";
- 
-      //   whatWasSpliced;
-      //   console.log("Updated Array: ", toArrayOfObjects)
-   
-        
-        // d--
-      //   console.log(localStorage.shoe);
-      //   toArrayOfObjects;
-          
-          
-        // for (let c = cartRowItems.length; c >= 0; c--) {
-        //   cartRowItems[d].style.display = "none";
-        // }
-
-        // for (let y = toArrayOfObjects.length - 1; y >= 0; y--) {
-        //   let itemThatWasSpliced = toArrayOfObjects.splice(y, 1);
-        //   console.log("Item that was spliced: ", itemThatWasSpliced);
-        //   console.log("Updated Array: ", toArrayOfObjects)
-        // }
-      //   if (toArrayOfObjects.length === 0) {
-      //     cartEmpty.style.display = "block";
-      //     orderSummaryBreakdown.style.display = "none";
-      //     localStorage.clear();
-      //   }
+        if (toArrayOfObjects.length === 0) {
+          cartEmpty.style.display = "block";
+          orderSummaryBreakdown.style.display = "none";
+          localStorage.clear();
+        }
       
-      // for (let m = 0; m < toArrayOfObjects.length; m++) {
-      //   let amountToSubtract = toArrayOfObjects[m].productPrice.slice(1);
-      //   // total.textContent = Number(total) - Number(amountToSubtract);
-      //   // console.log(typeof amountToSubtract, "amount to subtract :", amountToSubtract);
-      //   let newTotal = Number(total.textContent.slice(1)) - amountToSubtract;
-      //   // console.log("modified?: ", Number(total.textContent.slice(1)) - newTotal)
-      // }
-      // localStorage.setItem("shoe", JSON.stringify(newToArrayOfObjects));
-
-  
-  // console.log("Cart Delete Button: ", cartDeleteButton[d].parentNode.childNodes[0].textContent);
-  // let newToArrayOfObjects = toArrayOfObjects.filter(item => cartDeleteButton[d].parentNode.childNodes[0].textContent !== item.productName);
-  // console.log(newToArrayOfObjects);
-  
-// }
-
-
-
 
 let productPriceArray = document.querySelectorAll(".cart-product-price");
 let updatedPriceArray = [];
 for (let i = 0; i < productPriceArray.length; i++) {
   let individualProductPrice = productPriceArray[i].textContent.slice(1);
   updatedPriceArray.push(individualProductPrice);
-  // console.log("Individual Product Price: ", individualProductPrice);
 }
-// console.log("Updated Price Array: ", updatedPriceArray);
 
 let sum = 0;
 for (let s = 0; s < updatedPriceArray.length; s++) {
   sum += Number(updatedPriceArray[s]);
 }
-// console.log(sum);
 
 total.textContent = `$${sum}.00`;
